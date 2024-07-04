4325ec0c53c695e7703d1f9ea0a4706eb3d36bb2
Joseph Corral <447399989@student.sbhs.nsw.edu.au>
2024-07-04 10:49:08 +1000

    added delete button functionality

--- FitBoard/Program Modification/Program Modification.js
+++ FitBoard/Program Modification/Program Modification.js
@@ -1,5 +1,7 @@
 // #region JS
 
+// 's Work
+
 // #region Inner Button Logic
 
 // #region Row Buttons
@@ -18,7 +20,7 @@ function handleKeyDown(e) {
 
 function Edit_Row(button) {
     const Row = button.parentNode.parentNode;
-    const Cells = Row.getElementsByClassName('editable');
+    const Cells = Row.getElementsByClassName('Editable');
     for (let i = 0; i < Cells.length; i++) {
         const Cell = Cells[i];
         const Text = Cell.innerText;
@@ -32,13 +34,13 @@ function Edit_Row(button) {
     }
 
     button.style.display = 'none';
-    Row.querySelector('button[class="save"]').style.display = 'inline-block';
-    Row.querySelector('button[class="delete"]').style.display = 'inline-block';
+    Row.querySelector('button[class="Save"]').style.display = 'inline-block';
+    Row.querySelector('button[class="Delete"]').style.display = 'inline-block';
 }
 
 function Save_Row(button){
     const Row = button.parentNode.parentNode;
-    const Cells = Row.getElementsByClassName('editable');
+    const Cells = Row.getElementsByClassName('Editable');
 
     for (let i = 0; i <Cells.length; i++){
         const Cell = Cells[i];
@@ -49,8 +51,8 @@ function Save_Row(button){
     }
 
     button.style.display = 'none';
-    Row.querySelector('button[class="delete"]').style.display = 'none';
-    Row.querySelector('button[class="edit"]').style.display = 'inline-block';
+    Row.querySelector('button[class="Delete"]').style.display = 'none';
+    Row.querySelector('button[class="Edit"]').style.display = 'inline-block';
 
 }
 
@@ -60,23 +62,22 @@ function Delete_Row(button){
 }
 
 function Add_New(button, position){
-
     const Row = button.parentNode.parentNode;
     const New_Row = document.createElement('tr');
     let rowTemplate = `
         <td>
-            <span class="editable" contenteditable="false"></span>
+            <span class="Editable" contenteditable="false"></span>
         </td>
         <td>
-            <span class="editable" contenteditable="false"></span>
+            <span class="Editable" contenteditable="false"></span>
         </td>
         <td>
-            <span class="editable" contenteditable="false"></span>
+            <span class="Editable" contenteditable="false"></span>
         </td>
         <td>
-            <button class="edit" onclick="Edit_Row(this)">Edit</button>
-            <button class="save" onclick="Save_Row(this)" style="display: none;">Save</button>
-            <button class="delete" onclick="Delete_Row(this)" style="display: none;">Delete</button>
+            <button class="Edit" onclick="Edit_Row(this)">Edit</button>
+            <button class="Save" onclick="Save_Row(this)" style="display: none;">Save</button>
+            <button class="Delete" onclick="Delete_Row(this)" style="display: none;">Delete</button>
         </td>
     `;
     
@@ -84,12 +85,12 @@ function Add_New(button, position){
 
     const tbody = Row.parentNode;
     New_Row.setAttribute('draggable', true);
-    New_Row.addEventListener('dragstart', handleDragStart);
-    New_Row.addEventListener('dragenter', handleDragEnter);
-    New_Row.addEventListener('dragover', handleDragOver);
-    New_Row.addEventListener('dragleave', handleDragLeave);
-    New_Row.addEventListener('drop', handleDrop);
-    New_Row.addEventListener('dragend', handleDragEnd);
+    New_Row.addEventListener('dragstart', Handle_Drag_Start);
+    New_Row.addEventListener('dragenter', Handle_Drag_Enter);
+    New_Row.addEventListener('dragover', Handle_Drag_Over);
+    New_Row.addEventListener('dragleave', Handle_Drag_Leave);
+    New_Row.addEventListener('drop', Handle_Drop);
+    New_Row.addEventListener('dragend', Handle_Drag_End);
     if (position === 'above') {
         tbody.insertBefore(New_Row, Row);
     } else if (position === 'below'){
@@ -104,7 +105,7 @@ function Add_New(button, position){
 
 function Add_Day(button, position) {
   let table = button.closest('table');
-  let newTable = createNewDayTable();
+  let newTable = Create_New_Day_Table();
 
   if (position === 'above') {
     table.parentNode.insertBefore(newTable, table);
@@ -112,15 +113,17 @@ function Add_Day(button, position) {
     table.parentNode.insertBefore(newTable, table.nextSibling);
   }
 
-
   // Initialize drag-and-drop for new table rows
-  initializeDragAndDrop(newTable);
+  Initialise_Drag_And_Drop(newTable);
 
   // Update the days numbering for all tables
-  updateDaysNumbering();
+  Update_Days_Numbering();
+
+  // Checks if the delete day buttons should be disabled or not
+  Check_Delete_Buttons();
 }
 
-function createNewDayTable() {
+function Create_New_Day_Table() {
   let tableTemplate = `
     <table>
       <thead>
@@ -132,13 +135,13 @@ function createNewDayTable() {
         </tr>
       </thead>
       <tbody>
-        <tr>
+        <tr class = "Yoga">
           <td><u>Yoga</u></td>
           <td></td>
           <td></td>
           <td></td>
         </tr>
-        <tr>
+        <tr class = "Pre_Hab">
           <td><u>Pre-Hab</u></td>
           <td></td>
           <td></td>
@@ -146,7 +149,7 @@ function createNewDayTable() {
             <button class="New_Exercise" onclick="Add_New(this, 'below')">Add Exercise Below</button>
           </td>
         </tr>
-        <tr>
+        <tr class = "Main_Workout">
           <td><u>Main Workout</u></td>
           <td></td>
           <td></td>
@@ -155,7 +158,7 @@ function createNewDayTable() {
             <button class="New_Exercise" onclick="Add_New(this, 'below')">Add Exercise Below</button>
           </td>
         </tr>
-        <tr>
+        <tr class = "Stretches">
           <td><u>Stretches</u></td>
           <td></td>
           <td></td>
@@ -163,7 +166,7 @@ function createNewDayTable() {
             <button class="New_Exercise" onclick="Add_New(this, 'above')">Add Exercise Above</button>
           </td>
         </tr>
-        <tr>
+        <tr class = "Bottom_Row">
           <td></td>
           <td></td>
           <td></td>
@@ -188,25 +191,91 @@ function Delete_Day(button) {
   table.parentNode.removeChild(table);
   
   // Update the days numbering for all tables
-  updateDaysNumbering();
+  Update_Days_Numbering();
+
+  // Checks if the delete day buttons should be disabled or not
+  Check_Delete_Buttons();
 }
 
-function updateDaysNumbering() {
+function Update_Days_Numbering() {
   let tables = document.querySelectorAll('table');
   tables.forEach((table, index) => {
     table.querySelector('th.Col_1').innerText = `Day ${index + 1}`;
   });
 }
 
+function Check_Delete_Buttons(){
+    // Get all tables in the document
+    const tables = document.getElementsByTagName('table');
+    
+    // Get all buttons with the class 'Delete_Button'
+    let deleteButtons = document.getElementsByClassName('Delete_Day');
+    
+    if (tables.length === 1) {
+      console.log("One Table")
+        // If there is only one table, disable the Delete_Button in that table
+        for (let i = 0; i < deleteButtons.length; i++) {
+            deleteButtons[i].disabled = true;
+            
+        }
+    } else if (tables.length > 1) {
+      console.log("Multiple Tables")
+        // If there are multiple tables, enable all Delete_Buttons
+        for (let i = 0; i < deleteButtons.length; i++) {
+            deleteButtons[i].disabled = false;
+        }
+    }
+}
+
 // #endregion
 
+// #endregion
+
+// #region Outer Button Logic
+
+    function Save_Program(button){
+
+      /* 
+      Save button should:
+      Check Program Name is Valid and Exclusive
+      Check no reserved terms are being used
+      Group info in the same table together
+      Group exercises within each table between pre-hab, main workout and stretches
+      Check no duplicate exercise names are used in the same day
+      Check validity of exercise names
+      Check all exercise info has been filled in for each exercise
+      */      
+
+      let Program_Name = document.getElementById("Program_Name");
+      let Program_Sessions = document.getElementById("Program_Sessions");
+      let Program_Tier = document.getElementById("Program_Tier");
+      let tables = document.querySelectorAll('table');
+      
+      console.log("BEGIN");
+
+      console.log(Program_Name.value);
+      console.log(Program_Sessions.value);
+      console.log(Program_Tier.value);
+
+      tables.forEach((table) => {
+        Table_Num = table.querySelector('th.Col_1').innerText;
+        console.log(Table_Num)
+      });
+
+      console.log("END");
+    }
+
+    function Discard_Changes(button){
+
+    }
+    
 // #endregion
 
 // #region Dragging Logic
 
 let dragSrcEl = null;
   
-function handleDragStart(e) {
+function Handle_Drag_Start(e) {
     this.classList.add('dragging');
     dragSrcEl = this;
 
@@ -214,7 +283,7 @@ function handleDragStart(e) {
     e.dataTransfer.setData('text/html', this.innerHTML);
 }
 
-function handleDragOver(e) {
+function Handle_Drag_Over(e) {
     if (e.preventDefault) {
         e.preventDefault();
     }
@@ -224,15 +293,15 @@ function handleDragOver(e) {
     return false;
 }
 
-function handleDragEnter(e) {
+function Handle_Drag_Enter(e) {
     this.classList.add('dragover');
 }
 
-function handleDragLeave(e) {
+function Handle_Drag_Leave(e) {
     this.classList.remove('dragover');
 }
 
-function handleDrop(e) {
+function Handle_Drop(e) {
     if (e.stopPropagation) {
         e.stopPropagation();
     }
@@ -264,7 +333,7 @@ function handleDrop(e) {
     return false;
 }
 
-function handleDragEnd(e) {
+function Handle_Drag_End(e) {
     this.classList.remove('dragging');
 
     const rows = document.querySelectorAll('tr');
@@ -272,36 +341,24 @@ function handleDragEnd(e) {
 }
 
 
-function initializeDragAndDrop(element) {
+function Initialise_Drag_And_Drop(element) {
         const rows = element.querySelectorAll('tr');
         rows.forEach(row => {
-            row.addEventListener('dragstart', handleDragStart);
-            row.addEventListener('dragenter', handleDragEnter);
-            row.addEventListener('dragover', handleDragOver);
-            row.addEventListener('dragleave', handleDragLeave);
-            row.addEventListener('drop', handleDrop);
-            row.addEventListener('dragend', handleDragEnd);
+            row.addEventListener('dragstart', Handle_Drag_Start);
+            row.addEventListener('dragenter', Handle_Drag_Enter);
+            row.addEventListener('dragover', Handle_Drag_Over);
+            row.addEventListener('dragleave', Handle_Drag_Leave);
+            row.addEventListener('drop', Handle_Drop);
+            row.addEventListener('dragend', Handle_Drag_End);
         });
-    }
+  }
     
-    // Initialize drag-and-drop for all existing rows on page load
-    document.addEventListener('DOMContentLoaded', () => {
-        initializeDragAndDrop(document);
-    });
+// Initialize drag-and-drop for all existing rows on page load
+document.addEventListener('DOMContentLoaded', () => {
+    Initialise_Drag_And_Drop(document);
+});
 
 
-// #endregion
-
-// #region Outer Button Logic
-
-    function Save_Program(button){
-
-    }
-
-    function Discard_Changes(button){
-
-    }
-    
 // #endregion
 
 // #endregion
