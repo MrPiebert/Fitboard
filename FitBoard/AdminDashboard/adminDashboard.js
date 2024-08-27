7b71571b29ec6606da51e0412e28ce8bb80c17c8
Josh <111873936+JimbyJuice@users.noreply.github.com>
2024-08-27 21:26:22 +1000

    Update adminDashboard.js

--- FitBoard/AdminDashboard/adminDashboard.js
+++ FitBoard/AdminDashboard/adminDashboard.js
@@ -2,20 +2,6 @@ let editMode = false;
 
 /* On Page Load */
 function onload() {
-    var accessLevel = localStorage.getItem('accessLevel');
-    var userNameElement = document.getElementById('userName');
-    var userRoleElement = document.getElementById('userRole');
-
-    if (accessLevel === '1') {
-        userNameElement.textContent = 'Rurt&#160Kich';
-        userRoleElement.textContent = 'Admin';
-    } else if (accessLevel === '0') {
-        var studentData = JSON.parse(localStorage.getItem('studentData'));
-        if (studentData && studentData.givenName && studentData.surname) {
-            userNameElement.textContent = studentData.givenName + '\u00A0' + studentData.surname;
-            userRoleElement.textContent = 'Student';
-        }
-    }
 
     fetchStudentData();
     document.getElementById('editToggle').checked = false;
