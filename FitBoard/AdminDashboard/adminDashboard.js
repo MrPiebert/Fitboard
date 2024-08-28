4fce7a48ff10d072d45853ec622a8f79ec57f636
Josh <111873936+JimbyJuice@users.noreply.github.com>
2024-08-28 17:11:05 +1000

    Update adminDashboard.js

--- FitBoard/AdminDashboard/adminDashboard.js
+++ FitBoard/AdminDashboard/adminDashboard.js
@@ -3,10 +3,25 @@ let editMode = false;
 /* On Page Load */
 function onload() {
 
+    var accessLevel = localStorage.getItem('accessLevel');
+    var userNameElement = document.getElementById('userName');
+    var userRoleElement = document.getElementById('userRole');
+
+    if (accessLevel === '1') {
+        userNameElement.textContent = 'Kurt&#160Rich';
+        userRoleElement.textContent = 'Admin';
+    } else if (accessLevel === '0') {
+        var studentData = JSON.parse(localStorage.getItem('studentData'));
+        if (studentData && studentData.givenName && studentData.surname) {
+            userNameElement.textContent = studentData.givenName + '\u00A0' + studentData.surname;
+            userRoleElement.textContent = 'Student';
+        }
+    }
     fetchStudentData();
     document.getElementById('editToggle').checked = false;
     document.querySelector(".edit-mode").style.display = "none";
 }
+
 
 /* Fetch Student Data */
 function fetchStudentData() {
